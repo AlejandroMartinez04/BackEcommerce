b2d053bfa8d55a71c9d85cf32dbdda0afcac4bd9
Alejandro M <diegomartinez1101@gmail.com>
2023-05-18 17:24:36 -0500

    Fix search by email each client

--- people/api.py
+++ people/api.py
@@ -3,11 +3,22 @@ from rest_framework import viewsets, permissions
 from .serializers import ClientSerializer, ProductSerializer
 
 
-
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        queryset = Client.objects.all()
+        search_query = self.request.query_params.get('search', None)  # Obtener el parámetro de búsqueda
+
+        if search_query:
+            # Filtrar por correo electrónico o ID del cliente
+            queryset = queryset.filter(email=search_query)
+        
+        return queryset
+
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
