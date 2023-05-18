d27c80d3ff63bbcf946bcb09639fd8466abd5a2d
Alejandro M <diegomartinez1101@gmail.com>
2023-05-18 15:31:50 -0500

    Fix cors

--- people/api.py
+++ people/api.py
@@ -1,15 +1,13 @@
 from .models import Client, Product
 from rest_framework import viewsets, permissions
 from .serializers import ClientSerializer, ProductSerializer
-from rest_framework import generics
-from django.db import connection
+
 
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ClientSerializer
-
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
--- people/models.py
+++ people/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class People(models.Model):
     fullname = models.CharField(max_length=70)
     contact = models.CharField(max_length=12)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
 
     class Meta:
